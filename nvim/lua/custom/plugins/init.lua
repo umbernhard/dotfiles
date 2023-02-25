74857d8098c2d3afc03f0a63cb075148c2c12029
Matt Bernhard <mdb92nc@gmail.com>
2023-02-24 20:10:28 -0500

    updating for some new configs

---
+++ nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,13 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+
+
+-- nvim color highlighting setup
+return {
+  'brenoprata10/nvim-highlight-colors',
+  config = function() 
+    require('nvim-highlight-colors').setup {}
+  end,
+}
